e441e8d70c91f4eede67f0d13ec4be36074db2c6
rjdm1324 <rjdm1324@ajou.ac.kr>
2020-12-30 09:04:13 +0900

    Movie Web with Apollo

---
+++ src/routes/Movie.js
@@ -0,0 +1,42 @@
+import { useMutation } from "@apollo/react-hooks";
+import { gql } from "apollo-boost";
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const LIKE_MOVIE = gql`
+  mutation toggleLikedMovie($id: Int!, $isLiked: Boolean!) {
+    toggleLikedMovie(id: $id, isLiked: $isLiked) @client
+  }
+`;
+
+const Container = styled.div`
+  height: 380px;
+  width: 100%;
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  border-radius: 7px;
+`;
+
+const Poster = styled.div`
+  background-image: url(${(props) => props.bg});
+  height: 100%;
+  width: 100%;
+  background-size: cover;
+  background-position: center center;
+`;
+
+export default ({ id, bg, isLiked }) => {
+  const [toggleLikedMovie] = useMutation(LIKE_MOVIE, {
+    variables: { id: parseInt(id), isLiked },
+  });
+  return (
+    <Container>
+      <Link to={`/${id}`}>
+        <Poster bg={bg} />
+      </Link>
+      <button onClick={toggleLikedMovie}>
+        {isLiked ? "unLike!" : "Like!"}
+      </button>
+    </Container>
+  );
+};
